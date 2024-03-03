40a35436600907b641cf4309b01863aa282b8372
Fabio D. Argañaraz <ing.fabio.arg@gmail.com>
2024-03-02 21:58:23 -0300

    ejercicio realiza2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
-// TODO: Completa tu código aquí ⬇️
+// Usando require para traer a express
+const express = require('express');
 
-//Se recomienda no editar ni eliminar la instancia del servidor.
-// Instancia del servidor
-const server = app.listen(PORT, () => {
-  console.log(`listening on port http://localhost:${PORT}`);
+// Creando la aplicación de Express
+const app = express();
+
+// Configurando el puerto pa' que corra el servidor de Express
+const PORT = process.env.PORT || 3000; // Si el puerto tá ocupado, lo podés cambiar acá
+
+// Usando app pa' responder a pedidos GET en la ruta raíz
+app.get('/', (req, res) => {
+  res.send('Hola mundo'); // Cambiado el mensaje a "Hola mundo" para pasar el test
 });
 
-// Exportación del servidor
+// Respondiendo a pedidos GET en la ruta /mensaje
+app.get('/mensaje', (req, res) => {
+  res.send('este es mi servidor usando express'); // Cambiado el mensaje a minúsculas y sin signos de puntuación
+});
+
+// Instanciando el servidor
+const server = app.listen(PORT, () => {
+  console.log(`Che, escuchá en el puerto http://localhost:${PORT}`);
+});
+
+// Exportando el servidor
 module.exports = server;
